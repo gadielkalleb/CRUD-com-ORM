241f04720d051448e3e651adb4f3b32903f7812e
Gadiel Kalleb <gadiel_kalleb@hotmail.com>
2018-06-11 07:47:00 -0300

    projeto pratico CRUD com ORM

---
+++ controller/pessoas.js
@@ -0,0 +1,4 @@
+exports.index = (req, res) => res.render('home',{ 
+  title: 'Bem vindo ao cadastro de projetos',
+  mensagem: 'Olá mundo'
+})
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+const pessoas = require('./routes/pessoas')
+
+const model = require('./model/index')
+
+app.use(express.static('public'))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use('/', (req, res, next) => {
+  // console.log(req.rawHeaders)
+  console.log(req.url)
+  next()
+})
+app.use('/', (req, res) => res.render('index', { title: 'pagina inicial'}))
+app.use('/pessoas', pessoas)
+
+app.listen(port, () => console.log('escutando app na porta '+port))
